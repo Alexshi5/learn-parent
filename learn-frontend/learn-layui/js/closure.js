2eddce78e4538540a007ec2a7e29b71bddd5c215
alexshi5 <alexshishupei@outlook.com>
2018-04-10 21:45:13 +0800

    commit some file

---
+++ learn-frontend/learn-layui/js/closure.js
@@ -0,0 +1,35 @@
+$(function () {
+    var d = new a1();
+    d.b();
+    /*var c = new a2();
+    c.b();
+    c.b();*/
+});
+
+//闭包：有权访问另一个函数作用域内变量的函数叫做闭包，只用内部函数才能访问外部函数的局部变量，通过闭包来当作内部函数与外部函数的桥梁
+
+/**
+ * 闭包一
+ */
+function a1() {
+    var i = 0;
+
+    function b() {
+        i++;
+        alert(i);
+    }
+
+    b();
+    b();
+}
+
+/**
+ * 闭包二
+ */
+function a2() {
+    var i = 0;
+    this.b = function () {
+        i++;
+        alert(i);
+    }
+}
---
+++ learn-frontend/learn-layui/js/index_.js
@@ -0,0 +1,8 @@
+window.onload=function () {
+    var buttonNode=document.getElementById('button');
+    alert(buttonNode);
+    var buttoValue=buttonNode.value;
+    alert(buttoValue);
+    var a;
+    alert(!a);
+}
