7f3118b42233f8639a3e310ef563bea5e2913d79
alexshi5 <alexshishupei@outlook.com>
2018-04-23 21:04:04 +0800

    commit some files

--- learn-frontend/learn-layui/js/index_.js
+++ learn-frontend/learn-layui/js/index_.js
@@ -9,7 +9,50 @@ window.onload=function () {
 }*/
 
 $(function () {
-    var a = 0;
-    var b = (0 == a || 1 == a) ? 'yes' : 'no';
-    alert(b);
+    /* var a = 0;
+     var b = (0 == a || 1 == a) ? 'yes' : 'no';
+     alert(b);*/
+
+    /* var val = '小明;';
+     var result = val.split(';');
+     alert(result.length + '==' + result[0]);*/
+    /*
+        var i = 10;
+        if (i > 5) {
+            i = i + 3;
+        }
+        alert(i);*/
+
+
+    /* if (true) {
+         alert(true);
+     }
+     if (false) {
+         alert(false)
+     }*/
+
+    /*var t = new test();
+    var f = t.testj();
+    alert(f);*/
+
+    for (var i = 0; i < 3; i++) {
+        var num = new test();
+        alert(num.number());
+    }
 });
+
+/*
+function test() {
+    var i = 10;
+
+    this.testj = function () {
+        return i;
+    }
+}*/
+
+function test() {
+    var num = 3;
+    this.number = function () {
+        return num;
+    }
+}
--- learn-thinkinjava/src/main/java/com/thinkinjava/chapter3/Number.java
+++ learn-thinkinjava/src/main/java/com/thinkinjava/chapter3/Number.java
@@ -1,5 +1,7 @@
 package com.thinkinjava.chapter3;
 
+import java.io.IOException;
+
 /**
  * author Alex
  * date 2018/4/7
@@ -7,5 +9,25 @@ package com.thinkinjava.chapter3;
  */
 class Number {
     int a;
+
+    public static void main(String[] args) throws IOException {
+       /* String s = "小明;";
+        String[] split = s.split(";");
+        System.out.println(split.length + "==" + split[0]);*/
+
+        /*Date date =new Date();
+        DateFormat format=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String format1 = format.format(date);
+        System.out.println(format1);*/
+
+
+        String s1 = "394|张文华|高一支三大队|E09.645.001.003| G5001绕城高速外线K85+652M";
+        String s2 = "394|张文华|高一支三大队|E09.645.001.003|G5001绕城高速外线K85+652M";
+        String s3 = "394,张文华,高一支三大队,E09.645.001.003, G5001绕城高速外线K85+652M";
+        String[] split1 = s1.split("\\|");
+        String[] split2 = s2.split("\\|");
+        String[] split3 = s3.split(",");
+        System.in.read();
+    }
 }
 
