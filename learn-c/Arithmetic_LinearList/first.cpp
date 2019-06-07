8f218ffd58139fb705d9a2a7430451e0d9777775
alexshi5 <alexshishupei@outlook.com>
2019-06-07 16:35:54 +0800

    commit some files

---
+++ learn-c/Arithmetic_LinearList/first.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void example1();
+
+int main(){
+	//
+	return 0;
+}
+
+//线性表的顺序存储结构
+#define MAXSIZE 20 //存储空间初始容量分配
+typedef int ElemType; //存储元素类型ElemType根据实际情况而定，这里假设为int
+void example1(){
+	ElemType data[MAXSIZE]; //定义数组存储数据元素个数最大为MAXSIZE
+	int length; //线性表的当前长度
+}
